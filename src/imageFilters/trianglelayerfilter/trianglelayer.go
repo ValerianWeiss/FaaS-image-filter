06c873444d348afbbce52d2b471b64a4487bc46f
ValerianWeiss <valerian97@web.de>
2018-09-03 00:01:37 +0200

    fixed triangle filter

--- src/imageFilters/trianglelayerfilter/trianglelayer.go
+++ src/imageFilters/trianglelayerfilter/trianglelayer.go
@@ -6,7 +6,6 @@ import (
 	"image"
 	"io/ioutil"
 	"log"
-	"math"
 	"os"
 	"sync"
 )
@@ -40,10 +39,9 @@ func addLayerTriangle(baseImg image.Image, trImg image.Image) *image.RGBA {
 	centerY := trHeight / 2
 	edgeLen := edgeLen(trWidth, trHeight)
 
-	h := int(math.Sqrt(math.Pow(float64(edgeLen), 2) - math.Pow(float64(edgeLen/2), 2)))
-	p1 := []int{centerX - edgeLen/2, centerY + h/2}
-	p2 := []int{centerX, centerY - h/2}
-	p3 := []int{centerX + edgeLen/2, centerY + h/2}
+	p1 := []int{centerX - edgeLen/2, centerY + edgeLen/2}
+	p2 := []int{centerX, centerY - edgeLen/2}
+	p3 := []int{centerX + edgeLen/2, centerY + edgeLen/2}
 
 	f1 := func(x int) int {
 		return ((p1[1]-p2[1])/(p1[0]-p2[0]))*x + (p2[1]*p1[0]-p1[1]*p2[0])/(p1[0]-p2[0])
@@ -55,7 +53,7 @@ func addLayerTriangle(baseImg image.Image, trImg image.Image) *image.RGBA {
 
 	for x := 0; x < trWidth; x++ {
 		for y := 0; y < trHeight; y++ {
-			if y > f1(x) && y > f2(x) {
+			if y >= f1(x) && y >= f2(x) {
 				c := trImg.At(x, y)
 				newImg.Set(inPosX+x, inPosY+y, c)
 			}
